a1d57239f629d8503f2c7839583faf02c6f644b2
UrosObucina <urkeo196@gmail.com>
2018-11-29 11:30:55 +0100

    Tag Repository

--- html/typo3/typo3conf/ext/simpleblog/Classes/Domain/Repository/TagRepository.php
+++ html/typo3/typo3conf/ext/simpleblog/Classes/Domain/Repository/TagRepository.php
@@ -11,5 +11,5 @@ namespace Blog\Simpleblog\Domain\Repository;
 
 class TagRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 {
-
+    protected $defaultOrderings = array('tagvalue' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING);
 }
