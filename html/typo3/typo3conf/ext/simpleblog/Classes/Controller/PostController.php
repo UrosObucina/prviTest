4c54912dda8a1af8b513b279ae5fd3b2f0d4a967
UrosObucina <urkeo196@gmail.com>
2018-11-29 09:30:59 +0100

    Post Branch Commit

--- html/typo3/typo3conf/ext/simpleblog/Classes/Controller/PostController.php
+++ html/typo3/typo3conf/ext/simpleblog/Classes/Controller/PostController.php
@@ -84,5 +84,5 @@ class PostController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
         $post->postRepository->update($post);
         $this->reditect('show','Blog',NULL,array('blog'=>$blog));
     }
-    public function Brench2222(){}
+    public function Funkcija(){}
 }
